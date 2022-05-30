a028dbfc3f5b0fc03929357a57b6e4d68735d8f8
thomzera <93238590+thomzera@users.noreply.github.com>
2022-05-30 19:01:15 -0300

    Instalando o corsheaders

--- project/settings.py
+++ project/settings.py
@@ -45,17 +45,21 @@ INSTALLED_APPS = [
     'main',
     'localizacao',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'project.urls'
 
