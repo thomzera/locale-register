9fc3d962329c71ace0cd698ce01c13768527b195
thomzera <93238590+thomzera@users.noreply.github.com>
2022-05-30 12:12:13 -0300

    Implementando consulta dos registros

--- localizacao/urls.py
+++ localizacao/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import ListaLocalizacaoView, LocalizacaoCreateView
+from .views import (ListaLocalizacaoView, LocalizacaoCreateView,
+                    LocalizacaoDeleteView, LocalizacaoUpdateView)
 
 urlpatterns = [
     path('', ListaLocalizacaoView.as_view(), name='localizacao.index'),
-    path('novo/', LocalizacaoCreateView.as_view(), name='localizacao.novo')
+    path('novo/', LocalizacaoCreateView.as_view(), name='localizacao.novo'),
+    path('editar/<int:pk>', LocalizacaoUpdateView.as_view(), name='localizacao.editar'),
+    path('remover/<int:pk>', LocalizacaoDeleteView.as_view(), name='localizacao.remover'),
 ]
--- localizacao/views.py
+++ localizacao/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+# from django.shortcuts import render
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .forms import LocalizacaoForm
 from .models import Localizacao
@@ -9,9 +9,29 @@ from .models import Localizacao
 class ListaLocalizacaoView(ListView):
     model = Localizacao
     queryset = Localizacao.objects.all().order_by('nome')
- 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filtro_nome = self.request.GET.get('nome') or None
+
+        if filtro_nome:
+            queryset = queryset.filter(nome__contains=filtro_nome)
+
+        return queryset
+
 
 class LocalizacaoCreateView(CreateView):
     model = Localizacao
     form_class = LocalizacaoForm
+    success_url = '/localizacoes/'
+
+
+class LocalizacaoUpdateView(UpdateView):
+    model = Localizacao
+    form_class = LocalizacaoForm
+    success_url = '/localizacoes/'
+
+
+class LocalizacaoDeleteView(DeleteView):
+    model = Localizacao
     success_url = '/localizacoes/'
