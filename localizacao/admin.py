1f146395c29afe88203c49ae872b9d049e5663a2
thomzera <93238590+thomzera@users.noreply.github.com>
2022-05-30 16:52:35 -0300

    Customizando o admin

--- localizacao/admin.py
+++ localizacao/admin.py
@@ -4,4 +4,12 @@ from .models import Localizacao
 
 # Register your models here.
 
-admin.site.register(Localizacao)
+class LocalizacaoAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome',
+        'latitude',
+        'longitude',
+        'ativa',
+    ]
+
+admin.site.register(Localizacao, LocalizacaoAdmin)
