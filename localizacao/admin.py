1009b8e18a5cfff437861d7d279b9ad8762568b9
thomzera <93238590+thomzera@users.noreply.github.com>
2022-05-30 16:59:46 -0300

    Incluindo filtros e funções no admin

--- localizacao/admin.py
+++ localizacao/admin.py
@@ -4,6 +4,15 @@ from .models import Localizacao
 
 # Register your models here.
 
+@admin.action(description="Ativar todas as localizações")
+def ativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=True)
+
+@admin.action(description="Desativar todas as localizações")
+def desativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=False)
+
+
 class LocalizacaoAdmin(admin.ModelAdmin):
     list_display = [
         'nome',
@@ -11,5 +20,16 @@ class LocalizacaoAdmin(admin.ModelAdmin):
         'longitude',
         'ativa',
     ]
+    list_filter = [
+        'ativa'
+    ]
+    search_fields = [
+        'nome'
+    ]
+    actions = [
+        ativar_todos,
+        desativar_todos,
+    ]
+
 
 admin.site.register(Localizacao, LocalizacaoAdmin)
